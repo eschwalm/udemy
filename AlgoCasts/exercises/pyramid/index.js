4681aa602e098f8955e814b30f5949ecfd669a7c
Evan <schwalm.evan@gmail.com>
2018-03-09 12:14:58 -0800

    complete pyramid

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,32 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let baseSize = n * 2 - 1;
+  
+  for (let i = 0; i < n; i++) {
+    let step = '';
+    let buffer = '';
+    let bufferSize = 0;
+    
+    if (i === 0) {
+      step += '#';
+    } else {
+      while (step.length < (i * 2 + 1)) {
+        step += '#';
+      }
+    }
+    
+    if (step.length < baseSize) {
+      bufferSize = Math.floor((baseSize - step.length) / 2);
+      
+      while (buffer.length < bufferSize) {
+        buffer += ' ';
+      }
+    }
+    
+    console.log(buffer + step + buffer);    
+  }
+}
 
 module.exports = pyramid;
